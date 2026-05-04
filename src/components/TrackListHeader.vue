126716cae77c63042bc13be7b3079927175c5015
Dylan <dylan@tealforge.com>
2026-05-04 21:54:12 +1100

    Track list header: keep only Titre + Album, drop # and clock icon

--- src/components/TrackListHeader.vue
+++ src/components/TrackListHeader.vue
@@ -14,17 +14,12 @@ defineProps({
     :class="{ 'track-list-header--no-album': noAlbum }"
     aria-hidden="true"
   >
-    <span class="track-list-header-num">#</span>
+    <span></span>
     <span></span>
     <span>{{ t('track.col_title') }}</span>
     <span v-if="!noAlbum" class="track-list-header-album">{{ t('track.col_album') }}</span>
     <span></span>
-    <span class="track-list-header-duration">
-      <svg viewBox="0 0 24 24" width="14" height="14" fill="none" aria-hidden="true">
-        <circle cx="12" cy="12" r="9" stroke="currentColor" stroke-width="2"/>
-        <path d="M12 7v5l3 2" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-      </svg>
-    </span>
+    <span></span>
     <span></span>
   </div>
 </template>
